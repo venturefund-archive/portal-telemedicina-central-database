37c28043fdb343e8bf136a01f1f21af82cccf1d2
Guilherme Bauer Negrini <g.negrini@portaltelemedicina.com.br>
2022-07-20 17:18:30 -0300

    PDPP-1614_Add local database migration config to postCreateCommand

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -4,3 +4,7 @@ pip install -r central_database/requirements/local.txt
 
 # Install pre-commit hooks: https://pre-commit.com/. Amongst them is the auto-update of dependencies.
 pre-commit install -c central_database/.pre-commit-config.yaml
+
+# Configure the local postgres database
+python central_database/manage.py migrate
+python central_database/manage.py createsuperuser --username=admin --email=admin@admin.com
