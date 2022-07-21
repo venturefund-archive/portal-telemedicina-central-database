a5649cf2cd84aacf486ab509f08bdbe3a20383d7
Guilherme Bauer Negrini <g.negrini@portaltelemedicina.com.br>
2022-07-21 17:54:18 +0000

    PDPP-1614_Refactor postgres config and add Django superuser migration

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -6,5 +6,21 @@ pip install -r central_database/requirements/local.txt
 pre-commit install -c central_database/.pre-commit-config.yaml
 
 # Configure the local postgres database
+echo Applying migrations...
 python central_database/manage.py migrate
-python central_database/manage.py createsuperuser --username=admin --email=admin@admin.com
+echo Creating superuser...
+cat <<EOF | python central_database/manage.py shell
+from django.contrib.auth import get_user_model
+from os import environ
+
+USR=environ['DJANGO_SUPERUSER_USERNAME']
+EMAIL=environ['DJANGO_SUPERUSER_EMAIL']
+PSWD=environ['DJANGO_SUPERUSER_PASSWORD']
+
+User = get_user_model()  # Get the currently active user model,
+
+if not User.objects.filter(username=USR).exists():
+    User.objects.create_superuser(USR, EMAIL, PSWD)
+else:
+    print(f'User "{USR}" already exists, not created')
+EOF
