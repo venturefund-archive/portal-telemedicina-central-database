955800add113655fd5dd12fc2c7d5aa18d0475bf
Guilherme Bauer Negrini <g.negrini@portaltelemedicina.com.br>
2022-07-25 09:18:49 -0300

    Refactor: change to manage.py flush to clean the database

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -6,21 +6,9 @@ pip install -r central_database/requirements/local.txt
 pre-commit install -c central_database/.pre-commit-config.yaml
 
 # Configure the local postgres database
+echo Cleaning database...
+python central_database/manage.py flush --noinput
 echo Applying migrations...
 python central_database/manage.py migrate
 echo Creating superuser...
-cat <<EOF | python central_database/manage.py shell
-from django.contrib.auth import get_user_model
-from os import environ
-
-USR=environ['DJANGO_SUPERUSER_USERNAME']
-EMAIL=environ['DJANGO_SUPERUSER_EMAIL']
-PSWD=environ['DJANGO_SUPERUSER_PASSWORD']
-
-User = get_user_model()  # Get the currently active user model,
-
-if not User.objects.filter(username=USR).exists():
-    User.objects.create_superuser(USR, EMAIL, PSWD)
-else:
-    print(f'User "{USR}" already exists, not created')
-EOF
+python central_database/manage.py createsuperuser --noinput  # Usr: admin Pwd: admin
