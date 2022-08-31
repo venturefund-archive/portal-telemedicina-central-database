29341ec9b28859d9dfbb585221ac926c1c4c0e8d
Josue Silveira <j.silveira@ortaltelemedicina.com.br>
2022-08-31 19:55:49 +0000

    feat: add healthcheck route

--- central_database/config/urls.py
+++ central_database/config/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     # User management
     path("users/", include("central_database.users.urls", namespace="users")),
     path("accounts/", include("allauth.urls")),
-    # Your stuff: custom urls includes go here
+    path("health/", include("central_database.healthcheck.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 if settings.DEBUG:
     # Static file serving when using Gunicorn + Uvicorn
