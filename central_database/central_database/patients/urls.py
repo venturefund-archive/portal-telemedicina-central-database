a8db172d65207e438ff577ddefc55e9603f24527
Josue Silveira <j.silveira@portaltelemedicina.com.br>
2022-09-29 12:14:20 +0000

    fix: change route name to all patients

--- central_database/central_database/patients/urls.py
+++ central_database/central_database/patients/urls.py
@@ -2,7 +2,7 @@ from patients.views import PatientViewSet, VaccineStatusViewSet, VaccineViewSet
 from rest_framework import routers
 
 router = routers.DefaultRouter()
-router.register("patients", PatientViewSet, basename="patients")
+router.register("all", PatientViewSet, basename="patients")
 router.register("vaccines", VaccineViewSet, basename="vaccines")
 router.register("vaccine-status", VaccineStatusViewSet, basename="status")
 
