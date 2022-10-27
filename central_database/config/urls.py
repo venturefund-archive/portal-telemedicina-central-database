99c71097d1c6f5bf8deea1cc5de1fdbf808c8997
Josue Silveira <j.silveira@portaltelemedicina.com.br>
2022-10-27 20:32:45 +0000

    feat: add vaccine dose url

--- central_database/config/urls.py
+++ central_database/config/urls.py
@@ -21,6 +21,7 @@ urlpatterns = [
     path(settings.ADMIN_URL, admin.site.urls),
     # User management
     path("users/", include("central_database.users.urls", namespace="users")),
+    path("api/v1/vaccines/", include("vaccines.urls")),
     path("accounts/", include("allauth.urls")),
     path("health/", include("central_database.health.urls")),  # noqa
     # Your stuff: custom urls includes go here
