05185df94111b1425caf406b68a7a472d8490b0d
Josue Silveira <j.silveira@portaltelemedicina.com.br>
2022-10-27 20:29:43 +0000

    feat: add vaccine dose viewset

--- central_database/central_database/vaccines/views.py
+++ central_database/central_database/vaccines/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render  # noqa: F401
+from rest_framework.mixins import ListModelMixin, RetrieveModelMixin
+from rest_framework.viewsets import GenericViewSet
 
-# Create your views here.
+from central_database.vaccines.models import VaccineDose
+from central_database.vaccines.serializers import VaccineDosesSerializer
+
+
+class VaccineDosesViewSet(GenericViewSet, ListModelMixin, RetrieveModelMixin):
+
+    serializer_class = VaccineDosesSerializer
+
+    def get_queryset(self):
+        return VaccineDose.objects.all()
+
+    def get_serializer_context(self):
+        context = super().get_serializer_context()
+        parameter = self.request.query_params.get("patient_id")
+        context.update({"patient_id": parameter})
+        return context
