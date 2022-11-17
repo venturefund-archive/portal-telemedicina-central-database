b5df1311ffc0604f6df01835629f0c5a4f47ffea
Josue Silveira <j.silveira@portaltelemedicina.com.br>
2022-11-17 18:47:59 +0000

    chore: change import name

--- central_database/central_database/integrations/fhir_api/patient.py
+++ central_database/central_database/integrations/fhir_api/patient.py
@@ -1,18 +1,18 @@
 import requests
 
+from central_database.integrations.fhir_api.schema import fhir_url
+
 
 class PatientService:
     def __init__(self):
-        self.url = "http://172.18.0.6:8080/fhir/"
+        self.url = fhir_url
 
     def get_all(self):
         url = self.url + "Patient"
-        return requests.get(url).json()
+        data = requests.get(url)
+        return data.json()
 
     def get_detail(self, id):
         url = self.url + "Patient" + f"/{id}"
-        return requests.get(url).json()
-
-    def get_immunization(self, id):
-        url = self.url + f"/Immunization?patient={id}"
-        return requests.get(url).json()
+        data = requests.get(url)
+        return data.json()
