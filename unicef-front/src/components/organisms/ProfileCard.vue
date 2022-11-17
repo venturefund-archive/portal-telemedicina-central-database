7a09101f91f83bbb840b22acca81b091d5579fb5
Camila Rody <c.silva@portaltelemedicina.com.br>
2022-11-17 17:33:23 -0300

    feat: patints view has vaccine tables and some improvements

--- unicef-front/src/components/organisms/ProfileCard.vue
+++ unicef-front/src/components/organisms/ProfileCard.vue
@@ -1,17 +1,21 @@
 <template>
-  <div class="w-[30rem] rounded-xl border-2 border-b-4 border-gray-200 hover:bg-gray-50">
+  <div class="w-[30rem] rounded-xl border border-neutral-100 bg-gray-50">
     <div class="grid grid-cols-6 gap-y-2 p-5">
       <div>
         <img src="https://picsum.photos/seed/2/200/200" class="max-w-20 max-h-20 rounded-full" />
       </div>
 
       <div class="col-span-5 ml-4 md:col-span-4">
-        <p class="cursor-pointer font-bold text-gray-600 hover:underline">Joana Freitas Silva</p>
+        <p class="cursor-pointer font-bold text-gray-600 hover:underline">{{ profileForm.name }}</p>
         <div class="text-gray-400">
           <p>Blood Type: <span class="cursor-pointer hover:underline">B+</span></p>
           <p>Document: <span class="cursor-pointer hover:underline">064.964.967-53</span></p>
-          <p>Region: <span class="cursor-pointer hover:underline">Minas South</span></p>
-          <p>Contact: <span class="cursor-pointer hover:underline">(00) 00 000-000</span></p>
+          <p>
+            Region: <span class="cursor-pointer hover:underline">{{ profileForm.city }} / {{ profileForm.state }}</span>
+          </p>
+          <p>
+            Contact: <span class="cursor-pointer hover:underline">{{ profileForm.phone }}</span>
+          </p>
         </div>
       </div>
       <div class="col-start-2 ml-4 flex md:col-start-auto md:ml-0 md:justify-end">
@@ -19,4 +23,42 @@
       </div>
     </div>
   </div>
+
 </template>
+
+<script setup>
+import { onMounted, reactive } from 'vue'
+import InputIconWrapper from '@/components/InputIconWrapper.vue'
+import { MailIcon, LockClosedIcon, LoginIcon, UserIcon } from '@heroicons/vue/outline'
+import axios from 'axios'
+import { useRouter } from 'vue-router'
+import { useStorage } from '@vueuse/core'
+import { errorToast, successToast } from '@/toast'
+
+const router = useRouter()
+
+const profileForm = reactive({
+  // blood_type
+  // document
+  // age
+  name: '',
+  phone: '(00) 00 000-000',
+  state: '',
+  city: '',
+  processing: false,
+})
+
+const profile = async () => {
+  const state = useStorage('app-store', { token: '' })
+  try {
+    // const patient_id = 4172
+    // const response = await axios.post(import.meta.env.VITE_API_URL + `patients/${patient_id}`, profileForm)
+  } catch (err) {
+    errorToast({ text: err.message })
+  }
+}
+
+onMounted(() => {
+  profile()
+})
+</script>
