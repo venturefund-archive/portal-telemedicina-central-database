b5c7081569babf55486ecf31896c014a8f550a05
Josue Silveira <j.silveira@portaltelemedicina.com.br>
2022-11-21 17:14:47 +0000

    fix: change vue router history

--- unicef-front/src/router/index.js
+++ unicef-front/src/router/index.js
@@ -1,10 +1,10 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import NProgress from 'nprogress'
 import routes from '@/router/routes'
 import { sidebarState } from '@/composables'
 
 const router = createRouter({
-  history: createWebHashHistory('unicef-front'),
+  history: createWebHistory(),
   routes,
 })
 
