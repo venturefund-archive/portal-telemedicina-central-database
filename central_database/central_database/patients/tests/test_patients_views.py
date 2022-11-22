d017793a294738069c7c1c662ec5fae25e564a8f
Josue Silveira <j.silveira@portaltelemedicina.com.br>
2022-11-22 17:57:32 +0000

    feat: add fixtures to test endpoints

--- central_database/central_database/patients/tests/test_patients_views.py
+++ central_database/central_database/patients/tests/test_patients_views.py
@@ -1,13 +1,9 @@
+import json
 from unittest import mock
 
-from rest_framework.response import Response
 from rest_framework.reverse import reverse
-from rest_framework.test import APIClient, APITestCase
+from rest_framework.test import APITestCase
 
-from central_database.patients.tests.mocked_fhir_responses import (
-    all_patients,
-    patient_detail,
-)
 from central_database.users.tests.factories import UserFactory
 
 
@@ -16,32 +12,62 @@ class TestPatientViewSet(APITestCase):
         self.user = UserFactory()
 
     @mock.patch(
-        "central_database.patients.api.views.patient_resource.service.PatientService.get_all"
+        "central_database.patients.api.views.patient_resource.service.PatientService.get_all"  # noqa: E501
     )
     def test_it_requires_authentication(self, mock_get):
-        mock_get.return_value = all_patients
+        mock_get.return_value = json.load(
+            open(
+                "central_database/patients/tests/fixtures/all_patients_fhir_responses.json"  # noqa: E501
+            )
+        )
         response = self.client.get(reverse("api:patients-list"))
 
         self.assertEqual(response.status_code, 403)
 
     @mock.patch(
-        "central_database.patients.api.views.patient_resource.service.PatientService.get_all"
+        "central_database.patients.api.views.patient_resource.service.PatientService.get_all"  # noqa: E501
     )
     def test_it_lists_patients(self, mock_get):
-        mock_get.return_value = all_patients
+        mock_get.return_value = json.load(
+            open(
+                "central_database/patients/tests/fixtures/all_patients_fhir_responses.json"  # noqa: E501
+            )
+        )
 
         self.client.force_authenticate(self.user)
         response = self.client.get(reverse("api:patients-list"))
 
         self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data), 20)
+        for data in response.data:
+            self.assertTrue("id" in data)
+            self.assertTrue("name" in data)
 
     @mock.patch(
-        "central_database.patients.api.views.patient_resource.service.PatientService.get_detail"
+        "central_database.patients.api.views.patient_resource.service.PatientService.get_detail"  # noqa: E501
     )
     def test_it_retrieves_patient_detail(self, mock_get):
-        mock_get.return_value = patient_detail
+        mock_get.return_value = json.load(
+            open(
+                "central_database/patients/tests/fixtures/patient_detail_fhir_response.json"  # noqa: E501
+            )
+        )
 
         self.client.force_authenticate(self.user)
-        response = self.client.get(reverse("api:patients-detail", kwargs={"pk": 4172}))
+        response = self.client.get(
+            reverse("api:patients-detail", kwargs={"pk": 4172})
+        )  # noqa: E501
 
         self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data), 7)
+        keys = [
+            "id",
+            "name",
+            "telecom",
+            "gender",
+            "birth_date",
+            "address",
+            "marital_status",
+        ]
+        for key in keys:
+            self.assertTrue(key in response.data)
