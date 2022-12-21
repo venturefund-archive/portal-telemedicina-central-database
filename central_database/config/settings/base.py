aa2f780fe0e3be59b58d2a8548ad9bdc60a9007f
Josue Silveira <j.silveira@portaltelemedicina.com.br>
2022-12-21 00:27:01 +0000

    feat: remove session authentication from rest

--- central_database/config/settings/base.py
+++ central_database/config/settings/base.py
@@ -299,7 +299,6 @@ SOCIALACCOUNT_FORMS = {
 # https://www.django-rest-framework.org/api-guide/settings/
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
-        "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.TokenAuthentication",
     ),
     "DEFAULT_PERMISSION_CLASSES": (
