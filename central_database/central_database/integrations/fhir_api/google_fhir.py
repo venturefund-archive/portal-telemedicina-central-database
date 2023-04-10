ba6844f32fc56f4dc4b41eecda12515140ad330a
Josue Silveira <j.silveira@portaltelemedicina.com.br>
2023-04-10 14:24:44 +0000

    feat: add custom fhir client and server

--- central_database/central_database/integrations/fhir_api/google_fhir.py
+++ central_database/central_database/integrations/fhir_api/google_fhir.py
@@ -1,47 +1,40 @@
-import json
-
-from fhirclient import client
+from django.conf import settings
+from fhirclient.client import FHIRClient
+from fhirclient.server import FHIRServer
 from google.auth.transport import requests as google_requests
 from google.oauth2 import service_account
 
 
-class GoogleFHIRClient(client.FHIRClient):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+class GoogleFHIRServer(FHIRServer):
+    def __init__(self, client, base_uri=None, state=None, custom_session=None):
+        # Call the parent constructor
+        super().__init__(client, base_uri, state)
 
-        # Load Google Healthcare API settings from Django settings
-        from django.conf import settings
+        if custom_session is not None:
+            credentials = service_account.Credentials.from_service_account_file(  # noqa: E501
+                f"{settings.CENTRAL_DATABASE_PATH}/integrations/fhir_api/credentials.json"  # noqa: E501
+            )
 
-        self.project_id = settings.HEALTHCARE_API_PROJECT_ID
-        self.location = settings.HEALTHCARE_API_PROJECT_LOCATION
-        self.dataset_id = settings.HEALTHCARE_API_DATASET_ID
-        self.fhir_store_id = settings.HEALTHCARE_API_FHIR_STORE_ID
-        credentials_path = f"{settings.CENTRAL_DATABASE_PATH}/integrations/fhir_api/credentials.json"  # noqa: E501
+            # credentials, _ = google.auth.default()
 
-        # Set up Google API credentials
-        credentials = service_account.Credentials.from_service_account_file(
-            credentials_path
+            scoped_credentials = credentials.with_scopes(
+                ["https://www.googleapis.com/auth/cloud-platform"]
+            )
+
+            session = google_requests.AuthorizedSession(scoped_credentials)
+            self.session = session
+
+
+class GoogleFHIRClient(FHIRClient):
+    def __init__(self, settings=None, state=None, save_func=lambda x: x):
+        super().__init__(settings=settings, state=state, save_func=save_func)
+        self.server = GoogleFHIRServer(
+            self, base_uri=settings["api_base"], custom_session=True
         )
-        # credentials, _ = google.auth.default()
-        scoped_credentials = credentials.with_scopes(
-            ["https://www.googleapis.com/auth/cloud-platform"]
-        )
-        self.session = google_requests.AuthorizedSession(scoped_credentials)
 
-    def request_json(self, path, nosign=False):
-        full_url = self.construct_resource_url(path)
 
-        headers = {
-            "Content-Type": "application/fhir+json;charset=utf-8",
-            "Accept": "application/fhir+json",
-        }
+# settings = {
+# 'app_id': 'my_web_app',
+# 'api_base': 'https://healthcare.googleapis.com/v1/projects/ptm-gestao-di-dev/locations/southamerica-east1/datasets/taruma_data/fhirStores/taruma_dev_immunization/fhir'  # noqa: E501
 
-        response = self.session.get(full_url, headers=headers)
-        response.raise_for_status()
-        return json.loads(response.text)
-
-    def construct_resource_url(self, path):
-        base_url = "https://healthcare.googleapis.com/v1"
-        url = f"{base_url}/projects/{self.project_id}/locations/{self.location}"  # noqa: E501
-        resource_path = f"{url}/datasets/{self.dataset_id}/fhirStores/{self.fhir_store_id}/fhir/{path}"  # noqa: E501
-        return resource_path
+# }
