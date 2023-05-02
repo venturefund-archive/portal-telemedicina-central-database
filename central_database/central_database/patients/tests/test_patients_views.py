aceae2398d81d453f41e32fe0bda76de5cca14a6
Josue Silveira <j.silveira@portaltelemedicina.com.br>
2023-05-02 19:44:31 +0000

    feat: add tests for patients endpoints

--- central_database/central_database/patients/tests/test_patients_views.py
+++ central_database/central_database/patients/tests/test_patients_views.py
@@ -1,68 +1,108 @@
-import json
 from unittest import mock
 
-from django.conf import settings
+from fhirclient.models.patient import Patient
 from rest_framework.reverse import reverse
 from rest_framework.test import APITestCase
 
+from central_database.customers.factories import ClientFactory
+from central_database.patients.tests.factories import PatientFactory
+from central_database.permissions_manager.models import Role
 from central_database.users.tests.factories import UserFactory
-from central_database.vaccines.tests.factories import VaccineProtocolFactory
+from central_database.vaccines.tests.factories import (
+    VaccineAlertFactory,
+    VaccineAlertTypeFactory,
+    VaccineDoseFactory,
+    VaccineProtocolFactory,
+)
 
 
 class TestPatientViewSet(APITestCase):
     def setUp(self):
         self.user = UserFactory()
+        base_role = Role.objects.get(slug="healthcare-manager")
+        self.role = base_role.create_role_from_base_role(
+            client=ClientFactory()
+        )  # noqa: E501
+        self.role.assign_to_user(self.user)
 
     @mock.patch(
-        "central_database.patients.api.views.patient_resource.service.PatientService.get_all"  # noqa: E501
+        "central_database.patients.api.views.PatientsViewSet.get_queryset"  # noqa: E501
     )
     def test_it_requires_authentication(self, mock_get):
-        mock_get.return_value = json.load(
-            open(
-                f"{settings.CENTRAL_DATABASE_PATH}/patients/tests/fixtures/all_patients_fhir_responses.json"  # noqa: E501
-            )
-        )
         response = self.client.get(reverse("api:patients-list"))
 
         self.assertEqual(response.status_code, 401)
 
     @mock.patch(
-        "central_database.patients.api.views.patient_resource.service.PatientService.get_all"  # noqa: E501
+        "central_database.patients.api.views.PatientsViewSet.get_queryset"  # noqa: E501
+    )
+    def test_it_requires_patient_view_permission(self, mock_get):
+        self.role.users.remove(self.user)
+        queryset = [PatientFactory() for _ in range(2)]
+        mock_get.return_value = queryset
+
+        self.client.force_authenticate(self.user)
+        response = self.client.get(reverse("api:patients-list"))
+
+        self.assertEqual(response.status_code, 403)
+
+    @mock.patch(
+        "central_database.patients.api.views.PatientsViewSet.get_queryset"  # noqa: E501
     )
     def test_it_lists_patients(self, mock_get):
-        VaccineProtocolFactory()
-        mock_get.return_value = json.load(
-            open(
-                f"{settings.CENTRAL_DATABASE_PATH}/patients/tests/fixtures/all_patients_fhir_responses.json"  # noqa: E501
-            )
+
+        queryset = [PatientFactory() for _ in range(2)]
+        self.vaccine_dose_1 = VaccineDoseFactory(
+            minimum_recommended_age=1,
+            maximum_recommended_age=2,
         )
+        self.vaccine_dose_2 = VaccineDoseFactory(
+            minimum_recommended_age=1,
+            maximum_recommended_age=2,
+        )
+
+        self.vaccine_alert_type = VaccineAlertTypeFactory()
+        self.vaccine_alert_1 = VaccineAlertFactory(
+            patient_id=queryset[0].id,
+            vaccine_dose=self.vaccine_dose_1,
+            alert_type=self.vaccine_alert_type,  # noqa: E501
+        )
+        self.vaccine_alert_2 = VaccineAlertFactory(
+            patient_id=queryset[1].id,
+            vaccine_dose=self.vaccine_dose_2,
+            alert_type=self.vaccine_alert_type,  # noqa: E501
+        )
+
+        self.protocol = VaccineProtocolFactory(
+            vaccine_doses=[self.vaccine_dose_1, self.vaccine_dose_2]
+        )
+        mock_get.return_value = queryset
 
         self.client.force_authenticate(self.user)
         response = self.client.get(reverse("api:patients-list"))
 
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(response.data), 20)
+        self.assertEqual(len(response.data), 2)
         for data in response.data:
             self.assertTrue("id" in data)
+            self.assertTrue("birth_date" in data)
             self.assertTrue("name" in data)
+            self.assertTrue("number_of_alerts_by_protocol" in data)
+            self.assertTrue("age_in_days" in data)
 
     @mock.patch(
-        "central_database.patients.api.views.patient_resource.service.PatientService.get_detail"  # noqa: E501
+        "central_database.patients.api.views.PatientsViewSet.get_object"  # noqa: E501
     )
-    def test_it_retrieves_patient_detail(self, mock_get):
-        mock_get.return_value = json.load(
-            open(
-                f"{settings.CENTRAL_DATABASE_PATH}/patients/tests/fixtures/patient_detail_fhir_response.json"  # noqa: E501
-            )
-        )
+    def test_it_retrieves_a_patient_detail(self, mock_get):
+        patient = PatientFactory()
+        mock_get.return_value = patient
 
         self.client.force_authenticate(self.user)
         response = self.client.get(
-            reverse("api:patients-detail", kwargs={"pk": 4172})
+            reverse("api:patients-detail", kwargs={"pk": patient.id})
         )  # noqa: E501
 
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(response.data), 7)
         keys = [
             "id",
             "name",
@@ -74,3 +114,120 @@ class TestPatientViewSet(APITestCase):
         ]
         for key in keys:
             self.assertTrue(key in response.data)
+
+
+class TestPatientUpdateViewSet(APITestCase):
+    def setUp(self):
+        self.user = UserFactory()
+        base_role = Role.objects.get(slug="healthcare-manager")
+        self.role = base_role.create_role_from_base_role(
+            client=ClientFactory()
+        )  # noqa: E501
+        self.role.assign_to_user(self.user)
+
+    @mock.patch(
+        "central_database.patients.api.views.PatientsViewSet.get_object"  # noqa: E501
+    )
+    def test_it_requires_patient_change_permission(self, mock_get):
+        self.role.users.remove(self.user)
+        patient = PatientFactory()
+        mock_get.return_value = patient
+
+        payload = {"gender": "male"}
+        self.client.force_authenticate(self.user)
+        response = self.client.patch(
+            reverse("api:patients-detail", kwargs={"pk": patient.id}),
+            data=payload,
+            format="json",
+        )  # noqa: E501
+
+        self.assertEqual(response.status_code, 403)
+
+    @mock.patch(
+        "central_database.patients.api.views.PatientsViewSet.get_object"
+    )  # noqa: E501
+    @mock.patch.object(Patient, "update")
+    def test_it_updates_patient(self, mock_update, mock_get_object):
+        test_patient = PatientFactory()
+
+        mock_get_object.return_value = test_patient
+
+        payload = {
+            "resource_type": "Patient",
+            "id": test_patient.id,
+            "birth_date": "2023-01-06",
+            "name": "TEST PATIENT NAME",
+            "gender": "female",
+            "telecom": None,
+            "marital_status": None,
+            "address": [
+                {
+                    "id": 1,
+                    "line": ["123 Main St"],
+                    "city": "Anytownaa",
+                    "state": "CA",
+                    "postal_code": "68232422",
+                    "country": "BRAZIL",
+                    "latitude": 100,
+                    "longitude": 100,
+                }
+            ],
+        }
+
+        self.client.force_authenticate(self.user)
+        response = self.client.patch(
+            reverse("api:patients-detail", kwargs={"pk": test_patient.id}),
+            data=payload,
+            format="json",
+        )
+
+        mock_update.update.assert_not_called()
+
+        self.assertEqual(response.status_code, 200)
+
+    @mock.patch(
+        "central_database.patients.api.views.PatientsViewSet.get_object"
+    )  # noqa: E501
+    @mock.patch.object(Patient, "update")
+    def test_it_updates_patient_partially(self, mock_update, mock_get_object):
+        test_patient = PatientFactory()
+
+        mock_get_object.return_value = test_patient
+
+        payload = {"gender": "male"}
+
+        self.client.force_authenticate(self.user)
+        response = self.client.patch(
+            reverse("api:patients-detail", kwargs={"pk": test_patient.id}),
+            data=payload,
+            format="json",
+        )
+
+        mock_update.update.assert_not_called()
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data["gender"], "male")
+
+    @mock.patch(
+        "central_database.patients.api.views.PatientsViewSet.get_object"
+    )  # noqa: E501
+    @mock.patch.object(Patient, "update")
+    def test_update_coordinates(self, mock_update, mock_get_object):
+        test_patient = PatientFactory()
+
+        mock_get_object.return_value = test_patient
+
+        payload = {"address": [{"id": 1, "latitude": 100, "longitude": 101}]}
+
+        self.client.force_authenticate(self.user)
+        response = self.client.patch(
+            reverse("api:patients-detail", kwargs={"pk": test_patient.id}),
+            data=payload,
+            format="json",
+        )
+
+        mock_update.update.assert_not_called()
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data["address"][0].get("latitude"), 100)
+        self.assertEqual(response.data["address"][0].get("longitude"), 101)
