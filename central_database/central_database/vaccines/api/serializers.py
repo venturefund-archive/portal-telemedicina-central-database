11ca453842d4ee518190eb9b0d61970267ef52a5
Josue Silveira <j.silveira@portaltelemedicina.com.br>
2023-05-15 12:23:53 +0000

    feat: add prefetch to vaccine dose endpoint

--- central_database/central_database/vaccines/api/serializers.py
+++ central_database/central_database/vaccines/api/serializers.py
@@ -12,9 +12,7 @@ from central_database.vaccines.models import (
 )
 
 
-class VaccineSerializer(
-    PermissionSerializerMixin, serializers.ModelSerializer
-):  # noqa: E501
+class VaccineSerializer(serializers.ModelSerializer):  # noqa: E501
     class Meta:
         model = Vaccine
         fields = "__all__"
@@ -26,9 +24,7 @@ class VaccineAlertSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 
-class VaccineDosesSerializer(
-    PermissionSerializerMixin, serializers.ModelSerializer
-):  # noqa: E501
+class VaccineDosesSerializer(serializers.ModelSerializer):  # noqa: E501
     alerts = serializers.SerializerMethodField()
     is_completed = serializers.SerializerMethodField(method_name="get_status")
 
@@ -46,17 +42,15 @@ class VaccineDosesSerializer(
         ]
 
     def get_alerts(self, vaccine_dose_instance):
-        patient_id = self.context.get("patient_id")
-        alerts = vaccine_dose_instance.get_vaccine_alerts(
-            patient_id=patient_id
-        )  # noqa: E501
+        alerts = vaccine_dose_instance.active_alerts
         return VaccineAlertSerializer(alerts, many=True).data
 
     def get_status(self, vaccine_dose_instance):
-        patient_id = self.context.get("patient_id")
-        status = vaccine_dose_instance.get_vaccine_status(
-            patient_id=patient_id
-        ).first()  # noqa: E501
+        status = (
+            vaccine_dose_instance.patient_status[0]
+            if vaccine_dose_instance.patient_status
+            else None
+        )
         if status:
             return status.completed
         return None
