66375cc6a46ae433f0ef7cf16519ac0e2fecc80d
Josue Silveira <j.silveira@portaltelemedicina.com.br>
2023-05-24 14:09:19 +0000

    feat: add microregion model

--- central_database/central_database/customers/factories.py
+++ central_database/central_database/customers/factories.py
@@ -1,6 +1,7 @@
 import factory.fuzzy
+from django.contrib.gis.geos import Polygon
 
-from central_database.customers.models import Client
+from central_database.customers.models import Client, MicroRegion
 
 
 class ClientFactory(factory.django.DjangoModelFactory):
@@ -11,3 +12,12 @@ class ClientFactory(factory.django.DjangoModelFactory):
 
     class Meta:
         model = Client
+
+
+class MicroRegionFactory(factory.django.DjangoModelFactory):
+    name = factory.Faker("pystr")
+    polygon = Polygon(((0, 0), (0, 1), (1, 1), (0, 0)))
+    client = factory.SubFactory(ClientFactory)
+
+    class Meta:
+        model = MicroRegion
